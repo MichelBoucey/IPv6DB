c050537856d493d8d4aef4723a61a0bb88443147
Michel Boucey <michel.boucey@cybervisible.fr>
2017-04-04 18:33:47 +0200

    Replace wreq by http-client for tests suite

--- tests/hspec.hs
+++ tests/hspec.hs
@@ -2,8 +2,10 @@
 
 import           Control.Lens
 import           Data.Aeson
-import           Data.Aeson.Lens
-import           Network.Wreq
+-- import           Data.Aeson.Lens
+import           Network.HTTP.Client
+import           Network.HTTP.Types.Status (statusCode)
+import           Data.Vector
 import           Test.Hspec
 
 import           Network.IPv6DB.Types
@@ -15,34 +17,134 @@ main = hspec $ do
 
         it "Creates the resource for the given address in a list named hosts with a JSON object in source field" $ do
 
-            r <- post "http://localhost:4446/ipv6db/v1/list/hosts/addresses/abcd::1234" (object [("ttl",Number 60),("source", object [("data",String "A B C D")])])
-            r ^. responseStatus . statusCode `shouldBe` 204
+          mngr <- newManager defaultManagerSettings
+          initReq <- parseRequest "http://localhost:4446/ipv6db/v1/list/hosts/addresses/abcd::1234"
+          let req = initReq { method = "POST", requestBody = RequestBodyLBS $ encode (object [("ttl", Null),("source", object [("data",String "A B C D")])]) }
+          res <- httpLbs req mngr
+          statusCode (responseStatus res) `shouldBe` 204
 
     describe "PUT /ipv6db/v1/list/hosts/addresses/abcd::1234" $
 
-        it "Replaces the current source field in the previous POSTed resource with new data" $ do
+        it "Updates the resource previously POSTed" $ do
 
-            r <- put "http://localhost:4446/ipv6db/v1/list/hosts/addresses/abcd::1234" (object [("ttl",Number 60),("source", object [("data",String "E F G H")])])
-            r ^. responseStatus . statusCode `shouldBe` 204
+          mngr <- newManager defaultManagerSettings
+          initReq <- parseRequest "http://localhost:4446/ipv6db/v1/list/hosts/addresses/abcd::1234"
+          let req = initReq { method = "PUT", requestBody = RequestBodyLBS $ encode (object [("ttl", Null),("source", object [("data",String "E F G H")])]) }
+          res <- httpLbs req mngr
+          statusCode (responseStatus res) `shouldBe` 204
 
     describe "GET /ipv6db/v1/list/hosts/addresses/abcd::1234" $
 
-        it "Gets the resource with its optional TTL counter" $ do
+        it "Gets the resource" $ do
 
-            r <- get "http://localhost:4446/ipv6db/v1/list/hosts/addresses/abcd::1234"
-            r ^.. responseBody . key "source" . key "data" `shouldBe` [String "E F G H"]
+          mngr <- newManager defaultManagerSettings
+          req <- parseRequest "http://localhost:4446/ipv6db/v1/list/hosts/addresses/abcd::1234"
+          res <- httpLbs req mngr
+          responseBody res `shouldBe` "{\"ttl\":null,\"list\":\"hosts\",\"address\":\"abcd::1234\",\"source\":{\"data\":\"E F G H\"}}"
 
     describe "DELETE /ipv6db/v1/list/hosts/addresses/abcd::1234" $
 
         it "Deletes the resource" $ do
 
-            r <- delete "http://localhost:4446/ipv6db/v1/list/hosts/addresses/abcd::1234"
-            r ^. responseStatus . statusCode `shouldBe` 204
-{-
+          mngr <- newManager defaultManagerSettings
+          initReq <- parseRequest "http://localhost:4446/ipv6db/v1/list/hosts/addresses/abcd::1234"
+          let req = initReq { method = "DELETE" }
+          res <- httpLbs req mngr
+          statusCode (responseStatus res) `shouldBe` 204
+
     describe "POST /ipv6db/v1/list/hosts/addresses" $
 
         it "Creates resources for the given list named hosts" $ do
 
-            r <- post "http://localhost:4446/ipv6db/v1/list/hosts/addresses" TODO...
-            r ^. responseStatus . statusCode `shouldBe` 204
--}
+          mngr <- newManager defaultManagerSettings
+          initReq <- parseRequest "http://localhost:4446/ipv6db/v1/list/hosts/addresses"
+          let req = initReq
+                      { method = "POST"
+                      , requestBody =
+                          RequestBodyLBS $
+                            encode $
+                              Array $
+                                fromList
+                                  [ object
+                                      [ ("address","abcd::1234")
+                                        , ("ttl", Null)
+                                        , ("source", object [ ("data", String "E F G H") ])
+                                      ]
+                                  , object
+                                      [ ("address","abcd::5678")
+                                      , ("ttl", Null)
+                                      , ("source", object [ ("data", String "I J K L") ])
+                                      ]
+                                  ]
+                       }
+          res <- httpLbs req mngr
+          statusCode (responseStatus res) `shouldBe` 204
+
+    describe "PUT /ipv6db/v1/list/hosts/addresses" $
+
+        it "Updates the resources previously POSTed" $ do
+
+          mngr <- newManager defaultManagerSettings
+          initReq <- parseRequest "http://localhost:4446/ipv6db/v1/list/hosts/addresses"
+          let req = initReq
+                      { method = "PUT"
+                      , requestBody =
+                          RequestBodyLBS $
+                            encode $
+                              Array $
+                                fromList
+                                  [ object
+                                      [ ("address","abcd::1234")
+                                        , ("ttl", Null)
+                                        , ("source", object [ ("data", String "H G F E") ])
+                                      ]
+                                  , object
+                                      [ ("address","abcd::5678")
+                                      , ("ttl", Null)
+                                      , ("source", object [ ("data", String "L K J I") ])
+                                      ]
+                                  ]
+                       }
+          res <- httpLbs req mngr
+          statusCode (responseStatus res) `shouldBe` 204
+
+    describe "GET /ipv6db/v1/list/hosts/addresses" $
+
+        it "Gets the resources previously POSTed and PUTed" $ do
+
+          mngr <- newManager defaultManagerSettings
+          initReq <- parseRequest "http://localhost:4446/ipv6db/v1/list/hosts/addresses"
+          let req = initReq
+                      { method = "GET"
+                      , requestBody =
+                          RequestBodyLBS $
+                            encode $
+                              Array $
+                                fromList
+                                  [ String "abcd::1234"
+                                  , String "abcd::5678"
+                                  ]
+                       }
+          res <- httpLbs req mngr
+          responseBody res `shouldBe` "[{\"ttl\":null,\"list\":\"hosts\",\"address\":\"abcd::1234\",\"source\":{\"data\":\"H G F E\"}},{\"ttl\":null,\"list\":\"hosts\",\"address\":\"abcd::5678\",\"source\":{\"data\":\"L K J I\"}}]"
+
+    describe "DELETE /ipv6db/v1/list/hosts/addresses" $
+
+        it "Deletes the resources previously POSTed and PUTed" $ do
+
+          mngr <- newManager defaultManagerSettings
+          initReq <- parseRequest "http://localhost:4446/ipv6db/v1/list/hosts/addresses"
+          let req = initReq
+                      { method = "DELETE"
+                      , requestBody =
+                          RequestBodyLBS $
+                            encode $
+                              Array $
+                                fromList
+                                  [ String "abcd::1234"
+                                  , String "abcd::5678"
+                                  ]
+                       }
+          res <- httpLbs req mngr
+          statusCode (responseStatus res) `shouldBe` 204
+
